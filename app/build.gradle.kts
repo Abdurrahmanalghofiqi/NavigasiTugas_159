4fe9aeb0a5a9a45ff24ca5011401f697645298bf
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2025-11-08 21:53:37 +0700

    error trus mass

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.navigasitugas_159"
         minSdk = 27
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
