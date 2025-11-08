e10482f035268fa77f2148f1da4d5c90382e497a
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2025-11-08 19:17:35 +0700

    Ini membuat daftar pasangan data (key–value)

--- app/src/main/java/com/example/navigasitugas_159/view/DataPeserta.kt
+++ app/src/main/java/com/example/navigasitugas_159/view/DataPeserta.kt
@@ -35,7 +35,7 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
         Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
         Pair(first = stringResource(id = R.string.alamt), second = "Yogyakarta")
-    )
+    ).
     Scaffold(
         topBar = {
             TopAppBar(
