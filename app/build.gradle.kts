0109678ce2399d735ce4fd86ceebd2c8c373e227
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2025-11-08 21:34:18 +0700

    perbaikan error berulang

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.navigasitugas_159"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.navigasitugas_159"
         minSdk = 27
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
